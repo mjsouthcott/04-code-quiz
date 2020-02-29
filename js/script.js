305f95b4dd738ff5cec62a071a8be85bf0d4128f
Matthew Southcott <mjsouthcott@gmail.com>
2020-02-29 18:44:46 -0500

    Make major changes

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let question1 = {
+    question: "Which of the following belongs to the set of the built-in objects that comes with a browser?",
+    answers: ["Browser Object Model", "Document Object Model", "Global JavaScript Models", "All of the above"],
+    correctAnswer: 3
+}
+
+let question2 = {
+    question: "What does the string object \"trim()\" method do?",
+    answers: ["Changes the string to lowercase characters", "Changes the string to uppercase characters", "Removes white space from the start and end of the string", "None of the above"],
+    correctAnswer: 2
+}
+
+let question3 = {
+    question: "What keyword should you use when declaring a variable?",
+    answers: ["def", "let", "var", "None of the above"],
+    correctAnswer: 1
+}
+
+let question4 = {
+    question: "In the array [\"white\", \"black\", \"custom\"], what is the index of the element whose value is \"black\"?"
+    answers: ["-1", "0", "1", "2"],
+    correctAnswer: 2
+}
+
+let question5 = {
+    question: "Which of the following is an arithmetic operator?",
+    answers: ["true", "&&", "!", "%"],
+    correctAnswer: 3
+}
