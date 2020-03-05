d5ef6c2c43fe140877c1b23c8d50f204ae641e7e
Matthew Southcott <mjsouthcott@gmail.com>
2020-03-05 09:21:56 -0500

    Add High Score List display and Clear High Scores functionality

--- high-scores/js/script.js
+++ high-scores/js/script.js
@@ -1,10 +1,20 @@
-let $highScoreList = $('#high-score-list')
-
-// TODO
 let highScores = JSON.parse(localStorage.getItem('highScores'))
 
 highScores.sort(function(a, b) {
-    return a.score - b.score
+    return b.score - a.score
 })
 
 console.log(highScores)
+
+for (let i = 0; i < highScores.length; i++) {
+    let highScoreMsg = '<li>' + (i + 1) + '. ' + highScores[i].initials + ' - ' + highScores[i].score + '</li>'
+    $('#high-score-list ul').append(highScoreMsg)
+}
+
+$('#clear-high-scores').on('click', function(e) {
+    e.preventDefault()
+    highScores = []
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    localStorage.setItem('numQuizzes', 0)
+    $('#high-score-list ul').html('')
+})
