251dd8d6a5587ddccc1b606fedfbf0f070e68fea
Matthew Southcott <mjsouthcott@gmail.com>
2020-03-06 22:09:24 -0500

    Add documentation to JS files

--- high-scores/js/script.js
+++ high-scores/js/script.js
@@ -1,16 +1,18 @@
+// Get highScores from localStorage
 let highScores = JSON.parse(localStorage.getItem('highScores'))
 
+// Sort highScores in descending order by score
 highScores.sort(function(a, b) {
     return b.score - a.score
 })
 
-console.log(highScores)
-
+// Display the high scores in the window
 for (let i = 0; i < highScores.length; i++) {
     let highScoreMsg = '<li>' + (i + 1) + '. ' + highScores[i].initials + ' - ' + highScores[i].score + '</li>'
     $('#high-score-list ul').append(highScoreMsg)
 }
 
+// Event listener for clear-high-scores button. Sets localStorage variables to 0/an empty array, clearing the high scores from the window.
 $('#clear-high-scores').on('click', function(e) {
     e.preventDefault()
     highScores = []
--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
+// Variables pointing to jQuery objects
 let $timer = $('#timer')
 let $content = $('#content')
 let $landing = $('#landing')
 let $question = $(document.createElement('div')).attr('id', 'question')
 let $answerFeedback = $(document.createElement('div')).attr('id', 'answer-feedback')
 let $enterHighScore = $(document.createElement('div')).attr('id', 'enter-high-score')
-    //let $initials = $('#initials')
 
-let timeRemaining = 60
+// Various constants/variables
+const timeRemaining = 60
 let questionIndex = 0
 let timerInterval
+
+// Variables in localStorage (only declared/initialized if they don't already exist)
 if (!localStorage.getItem('highScores')) {
     let highScores = []
     localStorage.setItem('highScores', JSON.stringify(highScores))
@@ -17,6 +20,7 @@ if (!localStorage.getItem('numQuizzes')) {
     localStorage.setItem('numQuizzes', 0)
 }
 
+// Function to randomize the question list. Accepts and returns an array.
 function randomizeQuestionList(questionList) {
     let currentIndex = questionList.length
     let randomIndex, temporaryValue
@@ -30,6 +34,7 @@ function randomizeQuestionList(questionList) {
     return questionList
 }
 
+// Function to start the quiz. Removes the landing container and replaces it with the first question container.
 function startQuiz() {
     $landing.remove()
     $content.prepend($question)
@@ -37,6 +42,7 @@ function startQuiz() {
     nextQuestion()
 }
 
+// Function to start the timer. Counts down from timeRemaining in 1000 ms intervals. Ends the quiz if timeRemaining reaches 0.
 function startTimer() {
     timerInterval = setInterval(function() {
         timeRemaining--
@@ -47,6 +53,7 @@ function startTimer() {
     }, 1000)
 }
 
+// Function to display the next question. Verifies that questionIndex is less than the length of questionList. Populates the HTML with content from the questionList question objects.
 function nextQuestion() {
     $question.html('')
     if (questionIndex < questionList.length) {
@@ -60,7 +67,7 @@ function nextQuestion() {
         endQuiz()
     }
 }
-
+// Function to end the quiz. Removes the question container and replaces it with the enter-high-score container. Clears timerInterval and displays the time when the quiz ended.
 function endQuiz() {
     $question.remove()
     clearInterval(timerInterval)
@@ -73,11 +80,14 @@ function endQuiz() {
     $content.prepend($enterHighScore)
 }
 
+// Event listener on the start-quiz button. Randomizes questionList and starts the quiz.
 $content.on('click', '#start-quiz', function() {
     randomizeQuestionList(questionList)
     startQuiz()
 })
 
+// Event listener on the question buttons. Provides feedback depending on if the answer selected by the user was correct or incorrect. If an incorrect answer reduces timeRemaining to a negative value, sets timeRemaining to 0 and ends the quiz.
+// Otherwise, it increases questionIndex by 1 and calls the nextQuestion function.
 $content.on('click', 'ol button', function(e) {
     e.preventDefault()
     $this = $(this)
@@ -105,12 +115,13 @@ $content.on('click', 'ol button', function(e) {
     }
 })
 
+
+// Event listener on the submit button. Gets variables from localStorage, updates their values, and sets them in localStorage. Then takes user to High Score List page.
 $content.on('click', '#submit', function(e) {
     e.preventDefault()
     let numQuizzes = localStorage.getItem('numQuizzes')
     highScores = JSON.parse(localStorage.getItem('highScores'))
     highScores.push(new Object())
-        // is there a way to use a jQuery object/method?
     highScores[numQuizzes].initials = document.getElementById('initials').value.toUpperCase()
     highScores[numQuizzes].score = timeRemaining
     localStorage.setItem('highScores', JSON.stringify(highScores))
